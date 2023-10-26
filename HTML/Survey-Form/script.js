f8f6dea9b897bfa215b4ffac37369d6fad045eca
BHAV GUPTA <148852388+mittalxdesecrater@users.noreply.github.com>
2023-10-26 16:54:37 +0530

    Update script.js

--- HTML/Survey-Form/script.js
+++ HTML/Survey-Form/script.js
@@ -1,16 +1,27 @@
 
 
 function validateAndSubmit() {
+
+    function getGeneder(radioInputs){
+        var selectedValue=null;
+        for (var i = 0; i < radioInputs.length; i++) {
+            if (radioInputs[i].checked) {
+                selectedValue = radioInputs[i].value;
+                break; // Exit the loop once a selection is found
+            }
+        }
+        return selectedValue
+    }
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
     const date = document.getElementById('date').value;
     const country = document.getElementById('country').value;
-    const gender = document.getElementById('gender').value;
+    const gender =  getGeneder(document.getElementsByName('gender')) 
     const profession = document.getElementById('profession').value;
     const email = document.getElementById('email').value;
     const number = document.getElementById('number').value;
 
-
+    
     // checkboxes group required
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const selectedFields = Array.from(checkboxes)
